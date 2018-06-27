82d99b204fa831fcd60f66b503ee8418683d9e38
huy0105 <huynv@nal.vn>
2018-06-27 10:24:05 +0700

    add  API to project

--- HuyShop.Web/Api/PostCategoryController.cs
+++ HuyShop.Web/Api/PostCategoryController.cs
@@ -1,25 +1,101 @@
-﻿using HuyShop.Service;
+﻿using HuyShop.Model.Models;
+using HuyShop.Service;
 using HuyShop.Web.Infrastructure.Core;
-using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
 namespace HuyShop.Web.Api
 {
+    [RoutePrefix("api/postCategory")]
+    [Authorize]
     public class PostCategoryController : ApiControllerBase
     {
-        IPostCategoryService _postCategoryService;
+        private IPostCategoryService _postCategoryService;
+
         public PostCategoryController(IErrorService errorService, IPostCategoryService postCategoryService)
             : base(errorService)
         {
             this._postCategoryService = postCategoryService;
         }
-
-        public HttpResponseMessage Create(HttpRequestMessage request, IPostCategoryService postCategoryService)
+        [Route("getall")]
+        public HttpResponseMessage Get(HttpRequestMessage request)
         {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var listCategory = _postCategoryService.GetAll();
 
-           
+                    response = request.CreateResponse(HttpStatusCode.OK, listCategory);
+                }
+                return response;
+            });
         }
 
+        public HttpResponseMessage Post(HttpRequestMessage request, PostCategory postCategory)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var category = _postCategoryService.Add(postCategory);
+                    _postCategoryService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.Created, category);
+                }
+                return response;
+            });
+        }
+
+        public HttpResponseMessage Put(HttpRequestMessage request, PostCategory postCategory)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Update(postCategory);
+                    _postCategoryService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.OK);
+                }
+                return response;
+            });
+        }
+
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id )
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _postCategoryService.Delete(id);
+                    _postCategoryService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.OK);
+                }
+                return response;
+            });
+        }
     }
 }
