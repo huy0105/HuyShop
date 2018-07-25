e28e09197a2b52115b152f759c6112c4097e58bb
Nguyen Viet Huy <huynv@nal.vn>
2018-07-25 14:48:25 +0700

    add pagination to project

--- HuyShop.Web/Api/ProductCategoryController.cs
+++ HuyShop.Web/Api/ProductCategoryController.cs
@@ -3,7 +3,9 @@ using HuyShop.Model.Models;
 using HuyShop.Service;
 using HuyShop.Web.Infrastructure.Core;
 using HuyShop.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -22,13 +24,24 @@ namespace HuyShop.Web.Api
         }
 
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pagesize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var reponseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var reponse = request.CreateResponse(HttpStatusCode.OK, reponseData);
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pagesize).Take(pagesize);
+
+                var reponseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = reponseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pagesize)
+                };
+                var reponse = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return reponse;
             });
         }
