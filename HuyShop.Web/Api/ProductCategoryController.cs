c1e6c386db64bfe49120f1a5c52e59f65f1d0f48
Nguyen Viet Huy <huynv@nal.vn>
2018-07-26 10:43:39 +0700

    add event search

--- HuyShop.Web/Api/ProductCategoryController.cs
+++ HuyShop.Web/Api/ProductCategoryController.cs
@@ -24,12 +24,12 @@ namespace HuyShop.Web.Api
         }
 
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pagesize = 20)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, string keyword, int page, int pagesize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
                 int totalRow = 0;
-                var model = _productCategoryService.GetAll();
+                var model = _productCategoryService.GetAll(keyword);
                 totalRow = model.Count();
                 var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pagesize).Take(pagesize);
 
